c5a2bddbb55ce99627f100adf909112f47afa71a
khrr19u863 <regina.hamsina@gmail.com>
2023-05-18 16:34:34 +0300

    ADD: script for get time and memory statistics

--- src/scripts/check_zram.sh
+++ src/scripts/check_zram.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-VM=$1
+VM1=$1
+VM2=$2
 
-DATA=$2
-SRC=/home/regina/bmstu/bmstu-diploma/src/data/$DATA
+DATA=$3
+SRC=/home/regina/bmstu/bmstu-diploma/src/scripts/data/$DATA
 DST=/home/user/data
 
 RESET_PATH=/sys/block/zram0/reset
 SIZE_PATH=/sys/block/zram0/disksize
 STAT_PATH=/sys/block/zram0/mm_stat
 
+VM1_STAT=/home/regina/bmstu/bmstu-diploma/src/scripts/vm1
+VM2_STAT=/home/regina/bmstu/bmstu-diploma/src/scripts/vm2
 
-ssh user@$VM "rm -rf data && mkdir data"
 
-scp $SRC user@$VM:$DST
+ssh user@$VM1 "rm -rf data && mkdir data"
 
-ssh root@$VM "echo 1 > $RESET_PATH && echo 1G > $SIZE_PATH"
-ssh root@$VM "dd if=$DST/$DATA of=/dev/zram0"
-ssh user@$VM "cat $STAT_PATH" > $3
+scp $SRC user@$VM1:$DST
+
+ssh root@$VM1 "echo 1 > $RESET_PATH && echo 1G > $SIZE_PATH"
+ssh root@$VM1 "dmesg -c"
+ssh root@$VM1 "dd if=$DST/$DATA of=/dev/zram0"
+
+ssh user@$VM1 "cat $STAT_PATH" > $VM1_STAT/mm_stat.txt
+ssh root@$VM1 "dmesg | grep zram" > $VM1_STAT/time.txt
+
+python3 get_stat.py 1 memory $VM1_STAT/mm_stat.txt > stat/vm1_memory.txt
+python3 get_stat.py 1 time $VM1_STAT/time.txt > stat/vm1_time.txt
+
+
+ssh user@$VM2 "rm -rf data && mkdir data"
+
+scp $SRC user@$VM2:$DST
+
+ssh root@$VM2 "echo 1 > $RESET_PATH && echo 1G > $SIZE_PATH"
+ssh root@$VM2 "dmesg -c"
+ssh root@$VM2 "dd if=$DST/$DATA of=/dev/zram0"
+
+ssh user@$VM2 "cat $STAT_PATH" > $VM2_STAT/mm_stat.txt
+ssh root@$VM2 "dmesg | grep zram" > $VM2_STAT/time.txt
+
+python3 get_stat.py 2 memory $VM2_STAT/mm_stat.txt > stat/vm2_memory.txt
+python3 get_stat.py 2 time $VM2_STAT/time.txt > stat/vm2_time.txt
--- src/scripts/remove_zram.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo $1 > /sys/class/zram-control/hot_remove
